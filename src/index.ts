438b665cc015788f69abc618d5ab202848c2613c
Alexander Harding <AlexHardingYT@gmail.com>
2026-06-05 03:18:30 -0400

    Added decimal value handler
    This Noah guy will be the death of me.

--- src/index.ts
+++ src/index.ts
@@ -42,29 +42,25 @@ client.on(Events.MessageCreate, async (message: Message) => {
 
     const content = message.content;
 
+
     const dice_stats = content.match(rollRegex);
 
     if (dice_stats == null)
         return;
 
+    if (content.includes(".")) {
+        message.reply("Cannot roll a fraction of a dice.");
+        return;
+    }
     let quantity = 1;
     let size = 0;
     let modifiers = "";
 
     if (dice_stats[1] !== undefined) {
 
-        if (dice_stats[1].includes(".")) {
-            message.reply("Cannot use decimal values");
-            return;
-        }
-
         quantity = parseInt(dice_stats[1] || "");
         if (quantity < 0)
             quantity = 0
-    }
-    if (dice_stats[2]?.includes(".")) {
-        message.reply("Cannot use decimal values");
-        return;
     }
     size = parseInt(dice_stats[2] || "");
     if (size < 1)
